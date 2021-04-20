fc271cdf9512b9bc98192a00cb4754a3d3c7da28
Valle12 <valentinkaczmarek@yahoo.de>
2021-04-20 23:49:25 +0200

    #4 First Draft for Client and ClientProtocol

--- src/net/client/Client.java
+++ src/net/client/Client.java
@@ -1,4 +1,16 @@
 package net.client;
 
 public class Client {
+    private ClientProtocol connection;
+    private final String ipAdr = "25.93.29.50";
+    private int id;
+
+    public void connect() {
+        connection = new ClientProtocol(ipAdr, this);
+        connection.start();
+    }
+
+    public int getID() {
+        return id;
+    }
 }
--- src/net/client/ClientProtocol.java
+++ src/net/client/ClientProtocol.java
@@ -1,4 +1,48 @@
 package net.client;
 
-public class ClientProtocol {
+import net.messages.Connect;
+import net.messages.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientProtocol extends Thread {
+    private Client client;
+    private Socket clientSocket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private boolean running = true;
+    private final int port = 12975;
+
+    public ClientProtocol(String ipAdr, Client client) {
+        this.client = client;
+        try {
+            this.clientSocket = new Socket(ipAdr, port);
+            this.out = new ObjectOutputStream(clientSocket.getOutputStream());
+            this.in = new ObjectInputStream(clientSocket.getInputStream());
+            this.out.writeObject(new Connect(client.getID()));
+            this.out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        while (running) {
+            try {
+                Message m = (Message) in.readObject();
+                switch (m.getMessageType()) {
+                    case CONNECT:
+                        break;
+                    default:
+                        break;
+                }
+            } catch (ClassNotFoundException | IOException e) {
+                break;
+            }
+        }
+    }
 }
