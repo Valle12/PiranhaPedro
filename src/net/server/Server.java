793bcca799c42f639f3bbff6aafa86197dad6075
Valle12 <valentinkaczmarek@yahoo.de>
2021-04-26 00:01:42 +0200

    #4 Updated Server classes to be able to connect, disconnect and send system messages. Filled the empty structures

--- src/net/server/Server.java
+++ src/net/server/Server.java
@@ -1,4 +1,65 @@
 package net.server;
 
+import net.messages.Message;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Server {
+    private static final int port = 12975;
+    private ServerSocket serverSocket;
+    private static final String ipAdr = "25.93.29.50";
+    private boolean running;
+    private ArrayList<ServerProtocol> clients = new ArrayList<>();
+    private ArrayList<Integer> clientIDs = new ArrayList<>();
+
+    public synchronized void addNewClientID(int id) {
+        clientIDs.add(id);
+    }
+
+    public synchronized void removeClientID(int id) {
+        clientIDs.remove(id);
+    }
+
+    public synchronized void removeServerProtocol(ServerProtocol sp) {
+        clients.remove(sp);
+    }
+
+    public synchronized void sendToAll(Message m) {
+        int index = 0;
+        boolean sendFail = false;
+        for (ServerProtocol i : clients) {
+            try {
+                i.sendToClient(m);
+            } catch (IOException e) {
+                index = clients.indexOf(i);
+                sendFail = true;
+                continue;
+            }
+        }
+        if (sendFail) {
+            clients.remove(index);
+        }
+    }
+
+    public void listen() {
+        running = true;
+        try {
+            serverSocket = new ServerSocket(Server.port);
+            while (running) {
+                Socket clientSocket = serverSocket.accept();
+                ServerProtocol clientThread = new ServerProtocol(clientSocket, this);
+                clients.add(clientThread);
+                clientThread.start();
+            }
+        } catch (IOException e) {
+            if (serverSocket != null && serverSocket.isClosed()) {
+                System.out.println("Server stopped.");
+            } else {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/net/server/ServerProtocol.java
+++ src/net/server/ServerProtocol.java
@@ -1,4 +1,85 @@
 package net.server;
 
-public class ServerProtocol {
+import net.messages.Connect;
+import net.messages.Disconnect;
+import net.messages.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ServerProtocol extends Thread{
+    private Socket socket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+    private Server server;
+    private boolean running = true;
+
+    public ServerProtocol(Socket socket, Server server) {
+        this.socket = socket;
+        this.server = server;
+        try {
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        running = false;
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendToClient(Message m) throws IOException {
+        out.writeObject(m);
+        out.flush();
+    }
+
+    public void run() {
+        try {
+            while (running) {
+                Message m = (Message) in.readObject();
+                switch (m.getMessageType()) {
+                    case CONNECT:
+                        int id = ((Connect)m).getID();
+                        server.addNewClientID(id);
+                        System.out.println("Client " + id + " connected.");
+                        break;
+                    case DISCONNECT:
+                        id = ((Disconnect)m).getID();
+                        server.removeClientID(id);
+                        server.removeServerProtocol(this);
+                        running = false;
+                        disconnect();
+                        System.out.println("Client " + id + " disconnected.");
+                        break;
+                    case SYSTEMMESSAGE:
+                        server.sendToAll(m);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            running = false;
+            if (socket.isClosed()) {
+                System.out.println("Socket was closed.");
+            } else {
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
