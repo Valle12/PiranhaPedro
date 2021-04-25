aa856da04beb87d025711f96172ab82eb0eeaa75
Valle12 <valentinkaczmarek@yahoo.de>
2021-04-25 23:58:04 +0200

    #2 Updated Client methods to be able to connect, disconnect and send system messages

--- src/net/client/Client.java
+++ src/net/client/Client.java
@@ -2,12 +2,24 @@ package net.client;
 
 public class Client {
     private ClientProtocol connection;
-    private final String ipAdr = "25.93.29.50";
+    private static final String ipAdr = "25.93.29.50";
     private int id;
 
+    public Client(int id) {
+        this.id = id;
+    }
+
     public void connect() {
-        connection = new ClientProtocol(ipAdr, this);
+        connection = new ClientProtocol(Client.ipAdr, this);
         connection.start();
+    }
+
+    public void disconnect() {
+        connection.disconnect();
+    }
+
+    public void sendMessage(String message) {
+        connection.sendMessage(message);
     }
 
     public int getID() {
--- src/net/client/ClientProtocol.java
+++ src/net/client/ClientProtocol.java
@@ -1,13 +1,14 @@
 package net.client;
 
 import net.messages.Connect;
+import net.messages.Disconnect;
 import net.messages.Message;
+import net.messages.SystemMessage;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class ClientProtocol extends Thread {
     private Client client;
@@ -15,16 +16,38 @@ public class ClientProtocol extends Thread {
     private ObjectOutputStream out;
     private ObjectInputStream in;
     private boolean running = true;
-    private final int port = 12975;
+    private static final int port = 12975;
 
     public ClientProtocol(String ipAdr, Client client) {
         this.client = client;
         try {
-            this.clientSocket = new Socket(ipAdr, port);
+            this.clientSocket = new Socket(ipAdr, ClientProtocol.port);
             this.out = new ObjectOutputStream(clientSocket.getOutputStream());
             this.in = new ObjectInputStream(clientSocket.getInputStream());
             this.out.writeObject(new Connect(client.getID()));
             this.out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        running = false;
+        try {
+            if (!clientSocket.isClosed()) {
+                out.writeObject(new Disconnect(client.getID()));
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String message) {
+        try {
+            if (!clientSocket.isClosed()) {
+                out.writeObject(new SystemMessage(message));
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -35,7 +58,8 @@ public class ClientProtocol extends Thread {
             try {
                 Message m = (Message) in.readObject();
                 switch (m.getMessageType()) {
-                    case CONNECT:
+                    case SYSTEMMESSAGE:
+                        System.out.println(((SystemMessage)m).getMessage());
                         break;
                     default:
                         break;
