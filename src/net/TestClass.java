c2c37c971248eaa6dcb8dcc02c353f1465e72e71
Valle12 <valentinkaczmarek@yahoo.de>
2021-04-26 00:02:47 +0200

    #6 Extended TestClass to connect with 3 clients, then send a system message, then disconnect client2 and then send a system message again. Main method for testing at the moment

--- src/net/TestClass.java
+++ src/net/TestClass.java
@@ -1,8 +1,46 @@
 package net;
 
+import net.client.Client;
+import net.server.Server;
+
 public class TestClass {
+    private Server server;
+
+    public TestClass() {
+        server = new Server();
+        new ServerListenThread().start();
+        try {
+            joinClients();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void joinClients() throws InterruptedException {
+        Thread.sleep(100);
+        Client client1 = new Client(0);
+        client1.connect();
+        Thread.sleep(100);
+        Client client2 = new Client(1);
+        client2.connect();
+        Thread.sleep(100);
+        Client client3 = new Client(2);
+        client3.connect();
+        Thread.sleep(100);
+        client1.sendMessage("Alle Clients connected");
+        Thread.sleep(100);
+        client2.disconnect();
+        Thread.sleep(100);
+        client1.sendMessage("Clients disconnected");
+    }
+
+    class ServerListenThread extends Thread {
+        public void run() {
+            server.listen();
+        }
+    }
 
     public static void main(String[] args) {
-
+        new TestClass();
     }
 }
