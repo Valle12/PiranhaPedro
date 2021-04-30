c87c1b68c8b5e2f03e74e7dbffab815c71f627c2
Valle12 <valentinkaczmarek@yahoo.de>
2021-04-30 18:30:51 +0200

    #2 Added new Messages in Client and ClientProtocol

--- src/net/client/Client.java
+++ src/net/client/Client.java
@@ -1,28 +1,38 @@
 package net.client;
 
+import game.Board;
+
 public class Client {
-    private ClientProtocol connection;
-    private static final String ipAdr = "25.93.29.50";
-    private int id;
+  private ClientProtocol connection;
+  private static final String ipAdr = "25.93.29.50";
+  private int id;
 
-    public Client(int id) {
-        this.id = id;
-    }
+  public Client(int id) {
+    this.id = id;
+  }
 
-    public void connect() {
-        connection = new ClientProtocol(Client.ipAdr, this);
-        connection.start();
-    }
+  // Messages
 
-    public void disconnect() {
-        connection.disconnect();
-    }
+  public void connect() {
+    connection = new ClientProtocol(Client.ipAdr, this);
+    connection.start();
+  }
 
-    public void sendMessage(String message) {
-        connection.sendMessage(message);
-    }
+  public void disconnect() {
+    connection.disconnect();
+  }
 
-    public int getID() {
-        return id;
-    }
+  public void sendMessage(String message) {
+    connection.sendMessage(message);
+  }
+
+  public void updateBoard(Board board, int index) {
+    connection.updateBoard(board, index);
+  }
+
+  // Getter
+
+  public int getID() {
+    return id;
+  }
 }
--- src/net/client/ClientProtocol.java
+++ src/net/client/ClientProtocol.java
@@ -1,9 +1,7 @@
 package net.client;
 
-import net.messages.Connect;
-import net.messages.Disconnect;
-import net.messages.Message;
-import net.messages.SystemMessage;
+import game.Board;
+import net.messages.*;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -11,62 +9,76 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class ClientProtocol extends Thread {
-    private Client client;
-    private Socket clientSocket;
-    private ObjectOutputStream out;
-    private ObjectInputStream in;
-    private boolean running = true;
-    private static final int port = 12975;
+  private Client client;
+  private Socket clientSocket;
+  private ObjectOutputStream out;
+  private ObjectInputStream in;
+  private boolean running = true;
+  private static final int port = 12975;
 
-    public ClientProtocol(String ipAdr, Client client) {
-        this.client = client;
-        try {
-            this.clientSocket = new Socket(ipAdr, ClientProtocol.port);
-            this.out = new ObjectOutputStream(clientSocket.getOutputStream());
-            this.in = new ObjectInputStream(clientSocket.getInputStream());
-            this.out.writeObject(new Connect(client.getID()));
-            this.out.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  public ClientProtocol(String ipAdr, Client client) {
+    this.client = client;
+    try {
+      this.clientSocket = new Socket(ipAdr, ClientProtocol.port);
+      this.out = new ObjectOutputStream(clientSocket.getOutputStream());
+      this.in = new ObjectInputStream(clientSocket.getInputStream());
+      this.out.writeObject(new Connect(client.getID()));
+      this.out.flush();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    public void disconnect() {
-        running = false;
-        try {
-            if (!clientSocket.isClosed()) {
-                out.writeObject(new Disconnect(client.getID()));
-                clientSocket.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  public void disconnect() {
+    running = false;
+    try {
+      if (!clientSocket.isClosed()) {
+        out.writeObject(new Disconnect(client.getID()));
+        clientSocket.close();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    public void sendMessage(String message) {
-        try {
-            if (!clientSocket.isClosed()) {
-                out.writeObject(new SystemMessage(message));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  public void sendMessage(String message) {
+    try {
+      if (!clientSocket.isClosed()) {
+        out.writeObject(new SystemMessage(message));
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    public void run() {
-        while (running) {
-            try {
-                Message m = (Message) in.readObject();
-                switch (m.getMessageType()) {
-                    case SYSTEMMESSAGE:
-                        System.out.println(((SystemMessage)m).getMessage());
-                        break;
-                    default:
-                        break;
-                }
-            } catch (ClassNotFoundException | IOException e) {
-                break;
-            }
-        }
+  public void updateBoard(Board board, int index) {
+    try {
+      if (!clientSocket.isClosed()) {
+        out.writeObject(new UpdateBoardMessage(board, index));
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
+
+  public void run() {
+    while (running) {
+      try {
+        Message m = (Message) in.readObject();
+        switch (m.getMessageType()) {
+          case SYSTEMMESSAGE:
+            System.out.println(((SystemMessage) m).getMessage());
+            break;
+          case UPDATEBOARD:
+            // TODO updateView();
+            // playTurn();
+            break;
+          default:
+            break;
+        }
+      } catch (ClassNotFoundException | IOException e) {
+        break;
+      }
+    }
+  }
 }
